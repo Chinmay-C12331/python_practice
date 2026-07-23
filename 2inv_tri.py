93b6765d38daa1f977fa4ebc2a6d06d60b66c8d4
Chinmay Choudhari <chinmaychoudhari510@gmail.com>
2026-07-23 19:46:28 +0530

    Add triangular pattern printing in 2inv_tri.py

---
+++ 2inv_tri.py
@@ -0,0 +1,14 @@
+n=int(input())
+num=-1
+j=0
+for i in range(0,n):
+    i=n-i
+    #print(i)
+    num+=1
+    #print(num)
+    if i==n:
+        j=(n*2)-1
+        print((((" ")*num)+("* ")*j))
+    else:
+        gap=(num*2)-2
+        print((" "*num)+("* "*i)+(" "*gap)+("* "*(i)))
