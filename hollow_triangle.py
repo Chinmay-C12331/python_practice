2389640d1cbe9c77fece3d4ea3903afe9c433163
Chinmay Choudhari <chinmaychoudhari510@gmail.com>
2026-08-17 18:12:59 +0530

    Add hollow triangle pattern printing script
    This script prints a hollow triangle pattern with a height of 7.

---
+++ hollow_triangle.py
@@ -0,0 +1,18 @@
+N = 7
+
+for i in range(0, N):
+
+    if i == 0: 
+
+        print('  '* (N-1) + '*')
+
+    elif i == N - 1:
+
+        print('* ' * N)
+
+    else: 
+        left_spaces = '  ' * (N -i-1)
+
+        hollow_spaces = ('  ' * ( i - 1))
+
+        print(left_spaces + '* ' + hollow_spaces + '*')
