cb91e4c34b7794ceb0969048b898551a5c006627
Chinmay Choudhari <chinmaychoudhari510@gmail.com>
2026-08-19 20:05:53 +0530

    Add string manipulation and number checks in stttt.py

---
+++ stttt.py
@@ -0,0 +1,11 @@
+a=234
+b=" .,\\., 456 \){,. "
+b=b.strip(" .,\){")
+print(b)
+c="aBcDEfgH"
+a=str(a)
+print(a.isdigit())
+print(a.startswith("2"))
+print(a.endswith("4"))
+print(c.upper())
+print(c.lower())
