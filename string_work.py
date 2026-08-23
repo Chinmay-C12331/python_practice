e379005721997458a9b25f0d820878abc1507935
Chinmay Choudhari <chinmaychoudhari510@gmail.com>
2026-08-23 17:59:23 +0530

    Add string manipulation examples in string_work.py
    Demonstrate string manipulation methods including strip, capitalize, title, and swapcase.

---
+++ string_work.py
@@ -0,0 +1,14 @@
+a=",.,., we are going on a trip tomorrow   ..,.,.,  "
+a=a.strip(" .,")
+a=a.capitalize()
+print(a)
+a=a.title()
+print(a)
+a=a.swapcase()
+print(a)
+
+#output
+
+#We are going on a trip tomorrow
+#We Are Going On A Trip Tomorrow
+#wE aRE gOING oN a tRIP tOMORROW
